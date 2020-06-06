ea1cb1d21451f2e966fe3a8239dadc983171a65c
sne <snezhana.syngur@gmail.com>
2020-06-06 23:45:44 +0200

    fix: Small changes

--- src/main.js
+++ src/main.js
@@ -245,7 +245,7 @@ const createTripEventsListTemplate = () => {
 };
 
 const createEvent = () => {
-  return `  <li class="trip-events__item">
+  return `<li class="trip-events__item">
   <div class="event">
     <div class="event__type">
       <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
