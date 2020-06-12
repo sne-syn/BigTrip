fbbc41a32ab71abc1705ae2d504cbb2ebd0ef347
sne <snezhana.syngur@gmail.com>
2020-06-12 17:55:31 +0200

    new: Create a stats template file

--- src/main.js
+++ src/main.js
@@ -3,35 +3,37 @@ import {
 } from './components/create-event-template.js';
 import {
   createDayTemplate
-} from './components/day-template.js'
+} from './components/day-template.js';
 import {
   createTripDaysContainerTemplate
 } from './components/days-container-template.js';
 import {
   createEventEditTemplate
-} from './components/edit-event-template'
+} from './components/edit-event-template.js';
 import {
   createTripEventsListTemplate
-} from './components/events-list-template'
+} from './components/events-list-template.js';
 import {
   createFilterTemplate
-} from './components/filter-template'
+} from './components/filter-template.js';
 import {
   createFirstNewEvent
-} from './components/first-event-template'
+} from './components/first-event-template.js';
 import {
   createMenuTemplate
-} from './components/menu-template'
+} from './components/menu-template.js';
 import {
   createNewEventTemplate
-} from './components/new-event-template'
+} from './components/new-event-template.js';
 import {
   createSortTemplate
-} from './components/sort-template'
+} from './components/sort-template.js';
 import {
   createMainRouteInfoTemplate
-} from './components/trip-main-template'
-
+} from './components/trip-main-template.js';
+import {
+  createStatsTemplate
+} from './components/stats-template';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -57,6 +59,7 @@ const dayElement = document.querySelector(`.day`);
 render(dayElement, createTripEventsListTemplate(), `beforeend`);
 const eventListElement = document.querySelector(`.trip-events__list`);
 
-render(tripSection, createFirstNewEvent(), `beforeend`);
-render(tripSection, createEventEditTemplate(), `beforeend`);
-render(tripSection, createNewEventTemplate(), `beforeend`);
+//render(tripSection, createFirstNewEvent(), `beforeend`);
+//render(tripSection, createEventEditTemplate(), `beforeend`);
+//render(tripSection, createNewEventTemplate(), `beforeend`);
+//render(tripSection, createStatsTemplate(), `beforeend`);
